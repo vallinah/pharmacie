c982a739a3a29fe5980bdb7e98e6add799c47a65
Mihary-Mandresy <miharymandresy12@gmail.com>
2024-12-30 11:39:51 +0300

    feat: ajout du methode listeClass

--- src/fn/Function.java
+++ src/fn/Function.java
@@ -2,13 +2,24 @@ package fn;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.Vector;
 
+import base.*;
 import base.connexe.Connexion;
 
 public class Function {
     
     public static Connexion dbConnect() throws Exception {
         return new Connexion("pharmacie");
+    }
+
+    public static Vector<Class<?>> listeClass() {
+        Vector<Class<?>> liste = new Vector<>();
+        liste.add(Categorie.class);
+        liste.add(Produit.class);
+        liste.add(SousCategorie.class);
+        liste.add(Type.class);
+        return liste;
     }
 
     public static Date dateByString(String dt) throws Exception {
